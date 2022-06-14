c67e6bf58c314a78a4705b1ea5ca10f64267c509
Diego Fornalha <diegofornalha@gmail.com>
2022-06-14 17:44:55 +0000

    gitpod

---
+++ build/precache-manifest.22a5d4a2077f5041b4bf00094c843f4f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a82beab20ad33eeca1e97b6a20b779c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5ce5194ebf9c21f5f3d",
+    "url": "/static/css/main.0e632e19.chunk.css"
+  },
+  {
+    "revision": "f1d3d52cb70c6a80652c",
+    "url": "/static/js/2.c526ec28.chunk.js"
+  },
+  {
+    "revision": "c5ce5194ebf9c21f5f3d",
+    "url": "/static/js/main.5bc2715b.chunk.js"
+  },
+  {
+    "revision": "bc7615e31f6ee92c18d9",
+    "url": "/static/js/runtime-main.b33eb1b0.js"
+  }
+]);
